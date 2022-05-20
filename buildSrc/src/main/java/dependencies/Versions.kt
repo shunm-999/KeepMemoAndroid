809a3d4fc6fdd153bda0a6f6064fb1a627d3ca1b
shunm-999 <shunmiyazaki7812@gmail.com>
2022-05-20 23:52:54 +0900

    test

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -4,7 +4,7 @@ object Versions {
     const val ktlint = "0.43.2"
 
     private const val versionMajor = 1
-    private const val versionMinor = 0
+    private const val versionMinor = 1
     private const val versionPatch = 0
 
     const val versionCode = versionMajor * 10000 + versionMinor * 100 + versionPatch
