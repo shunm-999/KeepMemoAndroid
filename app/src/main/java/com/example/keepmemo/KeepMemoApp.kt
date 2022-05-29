6b73527c2ab540bcc2c7603e35a21e8bd8ebac44
shunm-999 <shunmiyazaki7812@gmail.com>
2022-05-29 14:28:36 +0900

    :sparkles: #34 #36 Dialog,Permission要求処理の実装

--- app/src/main/java/com/example/keepmemo/KeepMemoApp.kt
+++ app/src/main/java/com/example/keepmemo/KeepMemoApp.kt
@@ -11,67 +11,116 @@ import androidx.compose.material.Text
 import androidx.compose.material.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.keepmemo.ui.component.AppDrawer
+import com.example.keepmemo.ui.component.CustomAlertDialog
+import com.example.keepmemo.ui.component.DialogType
+import com.example.keepmemo.ui.launch.LaunchScreen
+import com.example.keepmemo.ui.launch.MainActivityViewModel
 import com.example.keepmemo.ui.theme.KeepMemoTheme
-import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun KeepDemoApp() {
+fun KeepDemoApp(
+    mainActivityViewModel: MainActivityViewModel = hiltViewModel()
+) {
     KeepMemoTheme {
-        ProvideWindowInsets {
-            val systemUiController = rememberSystemUiController()
-            val darkIcons = MaterialTheme.colors.isLight
-            val systemBarColor = MaterialTheme.colors.surface
-            SideEffect {
-                systemUiController.setSystemBarsColor(systemBarColor, darkIcons = darkIcons)
-            }
+        val systemUiController = rememberSystemUiController()
+        val darkIcons = MaterialTheme.colors.isLight
+        val systemBarColor = MaterialTheme.colors.surface
+        SideEffect {
+            systemUiController.setSystemBarsColor(systemBarColor, darkIcons = darkIcons)
+        }
 
-            val navController = rememberNavController()
-            val navigationActions = rememberKeepMemoNavigationActions(navController)
+        val uiState by mainActivityViewModel.uiState.collectAsState()
 
-            val drawerState = rememberDrawerState(DrawerValue.Closed)
-            val coroutineScope = rememberCoroutineScope()
-
-            val navBackStackEntry by navController.currentBackStackEntryAsState()
-            val currentRoute =
-                navBackStackEntry?.destination?.route ?: KeepMemoNavigation.Home.route
-
-            ModalDrawer(
-                drawerContent = {
-                    AppDrawer(
-                        currentRoute = currentRoute,
-                        navigateToHome = navigationActions.navigateToHome,
-                        navigateToLicense = navigationActions.navigationToLicense,
-                        closeDrawer = { coroutineScope.launch { drawerState.close() } },
-                        modifier = Modifier
-                            .statusBarsPadding()
-                            .navigationBarsPadding()
-                    )
-                },
-                drawerState = drawerState,
-                gesturesEnabled = currentRoute in listOf(KeepMemoNavigation.Home.route)
-            ) {
-                Surface(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .statusBarsPadding()
-                        .navigationBarsPadding()
-                ) {
-                    KeepMemoNavGraph(
-                        navController = navController,
-                        navigationActions = navigationActions,
-                        openDrawer = { coroutineScope.launch { drawerState.open() } }
-                    )
+        when (uiState.launchScreen) {
+            LaunchScreen.SPLASH -> {
+                LaunchContent {
+                    mainActivityViewModel.onSplashCompleted()
                 }
             }
+            LaunchScreen.MAIN -> {
+                var openDialog by remember { mutableStateOf(true) }
+
+                val storagePermissionState = rememberMultiplePermissionsState(
+                    permissions = listOf(
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                )
+                if (storagePermissionState.allPermissionsGranted) {
+                    KeepMemoAppContent()
+                } else {
+                    if (openDialog) {
+                        CustomAlertDialog(dialogType = DialogType.STORAGE_PERMISSIONS) {
+                            openDialog = false
+                            storagePermissionState.launchMultiplePermissionRequest()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun LaunchContent(onDismissDialog: (Boolean) -> Unit) {
+    CustomAlertDialog(dialogType = DialogType.APPLICATION_PRIVACY_POLICY, onDismissDialog)
+}
+
+@Composable
+private fun KeepMemoAppContent() {
+    val navController = rememberNavController()
+    val navigationActions = rememberKeepMemoNavigationActions(navController)
+
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val coroutineScope = rememberCoroutineScope()
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute =
+        navBackStackEntry?.destination?.route ?: KeepMemoNavigation.Home.route
+
+    ModalDrawer(
+        drawerContent = {
+            AppDrawer(
+                currentRoute = currentRoute,
+                navigateToHome = navigationActions.navigateToHome,
+                navigateToLicense = navigationActions.navigationToLicense,
+                closeDrawer = { coroutineScope.launch { drawerState.close() } },
+                modifier = Modifier
+                    .statusBarsPadding()
+                    .navigationBarsPadding()
+            )
+        },
+        drawerState = drawerState,
+        gesturesEnabled = currentRoute in listOf(KeepMemoNavigation.Home.route)
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .statusBarsPadding()
+                .navigationBarsPadding()
+        ) {
+            KeepMemoNavGraph(
+                navController = navController,
+                navigationActions = navigationActions,
+                openDrawer = { coroutineScope.launch { drawerState.open() } }
+            )
         }
     }
 }
---
+++ app/src/main/java/com/example/keepmemo/ui/component/AlertDialog.kt
@@ -0,0 +1,101 @@
+package com.example.keepmemo.ui.component
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.keepmemo.R
+
+enum class DialogType {
+    APPLICATION_PRIVACY_POLICY,
+    STORAGE_PERMISSIONS
+}
+
+@Composable
+fun CustomAlertDialog(
+    dialogType: DialogType,
+    onDialogDismiss: (Boolean) -> Unit
+) {
+    when (dialogType) {
+        DialogType.APPLICATION_PRIVACY_POLICY -> {
+            BaseAlertDialog(
+                titleRes = R.string.dialog_title_application_privacy_policy,
+                textRes = R.string.dialog_message_application_privacy_policy,
+                confirmButtonRes = R.string.dialog_button_yes,
+                dismissButtonRes = R.string.dialog_button_close,
+                dismissEnabled = false,
+                onDialogDismiss = onDialogDismiss
+            )
+        }
+        DialogType.STORAGE_PERMISSIONS -> {
+            BaseAlertDialog(
+                titleRes = R.string.dialog_title_storage_permission,
+                textRes = R.string.dialog_message_storage_permission,
+                confirmButtonRes = R.string.dialog_button_yes,
+                dismissEnabled = false,
+                onDialogDismiss = onDialogDismiss
+            )
+        }
+    }
+}
+
+@Composable
+private fun BaseAlertDialog(
+    @StringRes titleRes: Int,
+    @StringRes textRes: Int,
+    @StringRes confirmButtonRes: Int,
+    @StringRes dismissButtonRes: Int? = null,
+    dismissEnabled: Boolean,
+    onDialogDismiss: (Boolean) -> Unit
+) {
+    AlertDialog(
+        modifier = Modifier.padding(20.dp),
+        onDismissRequest = {
+            if (dismissEnabled) {
+                onDialogDismiss(false)
+            }
+        },
+        title = {
+            Text(
+                text = stringResource(id = titleRes),
+                style = MaterialTheme.typography.h6
+            )
+        },
+        text = {
+            Text(
+                text = stringResource(id = textRes),
+                style = MaterialTheme.typography.body1
+            )
+        },
+        confirmButton = {
+            Text(
+                text = stringResource(id = confirmButtonRes),
+                style = MaterialTheme.typography.button,
+                color = MaterialTheme.colors.primary,
+                modifier = Modifier
+                    .padding(15.dp)
+                    .clickable { onDialogDismiss(true) }
+            )
+        },
+        dismissButton = if (dismissButtonRes == null) {
+            null
+        } else {
+            {
+                Text(
+                    text = stringResource(id = dismissButtonRes),
+                    style = MaterialTheme.typography.button,
+                    color = MaterialTheme.colors.primary,
+                    modifier = Modifier
+                        .padding(15.dp)
+                        .clickable { onDialogDismiss(false) }
+                )
+            }
+        }
+    )
+}
