d204166f2150ef239399da4edd27eb37f46fd18e
shunm-999 <shunmiyazaki7812@gmail.com>
2022-06-23 21:48:56 +0900

    :sparkles: #43 通知タップ時に、フルスクリーン画面を表示

--- app/src/main/java/com/example/keepmemo/util/DeviceUtil.kt
+++ app/src/main/java/com/example/keepmemo/util/DeviceUtil.kt
@@ -8,5 +8,7 @@ object DeviceUtil {
 
     fun isOreoOver(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
 
+    fun isOreoMR1Over(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1
+
     fun isSOrOver(): Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
 }
