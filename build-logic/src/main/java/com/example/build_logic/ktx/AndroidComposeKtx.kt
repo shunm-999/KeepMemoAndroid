bba7b96faa86b0145ed96c5b65717a392d7764ef
shunm-999 <shunmiyazaki7812@gmail.com>
2022-11-14 20:59:59 +0900

    :up: #108 composeBomの導入

--- build-logic/src/main/java/com/example/build_logic/ktx/AndroidComposeKtx.kt
+++ build-logic/src/main/java/com/example/build_logic/ktx/AndroidComposeKtx.kt
@@ -3,6 +3,7 @@ package com.example.build_logic.ktx
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
 
 internal fun Project.configureAndroidCompose(
@@ -14,7 +15,12 @@ internal fun Project.configureAndroidCompose(
             compose = true
         }
         composeOptions {
-            kotlinCompilerExtensionVersion = libs.findVersion("androidx-compose-core").get().toString()
+            kotlinCompilerExtensionVersion = libs.findVersion("androidx-compose-compiler").get().toString()
+        }
+        dependencies {
+            val bom = libs.findLibrary("androidx-compose-bom").get()
+            add("implementation", platform(bom))
+            add("androidTestImplementation", platform(bom))
         }
     }
 }
